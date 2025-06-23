95eceb84e00dddbed78cf01ecfed5d0bc38e2745
Alvix.exe <alvix.wick@gmail.com>
2025-06-22 23:18:24 -0400

    Added new fields to the table to continue experimenting and learning.

--- state/models/state_property.py
+++ state/models/state_property.py
@@ -5,3 +5,23 @@ class StateProperty(models.Model):
     _description = 'State Property'
 
     name = fields.Char()
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ],
+        string='Garden Orientation'
+    )
