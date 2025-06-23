0e0f0c1967cacad0c8a0a3f0a91aad47827a6d8d
Alvix.exe <alvix.wick@gmail.com>
2025-06-22 20:14:27 -0400

    Fixed the '**manifest**.py' file to make the module work, following the documentation as a guide.

--- state/__manifest__.py
+++ state/__manifest__.py
@@ -1,4 +1,18 @@
 {
-    'name': 'State',
+    'name': "State",
+    'version': '1.0',
     'depends': ['base'],
+    'author': "Alvix.exe",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
 }
